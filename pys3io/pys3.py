481787e0b329570d8c39dadec5f210d685aa85c1
LeeAdtheorent <lee@adtheorent.com>
2016-02-01 12:10:24 -0500

    fixing local to s3

--- pys3io/pys3.py
+++ pys3io/pys3.py
@@ -15,35 +15,39 @@ Required config dictionary
 
 class PyS3:
     """
-    This is a BOTO wrapper (https://github.com/boto/boto)  that includes the following methods:
+    A very helpful python -- S3 (AWS) input output that includes reading and streaming files line by line
+
 
     ListFolder(self,folder):
     StreamFile(self,file_path):
     DwnldFile(self,S3_file_path, local_file_path):
     CloseConnection(self):
+
+    #Example of S3 setting configs:
+    s3_conf_dict = {
+                        'aws_access_key_id' : 'your_access_key',
+                        'aws_secret_access_key' : 'your_secret_key'
+                        }
+
     """
 
-    def __init__(self, bucket_name, config_file = 'etc\config.ini'):
+    def __init__(self, s3_conf_dict,bucket_name):
         """Instantiate the insance and open an active connection.
         """
         # Loading BOTO:
         import boto
-        # Setting configs:
-        configs = configparser.ConfigParser()
-        configs.read(config_file)
-        # Saving input var from the config file as a dictionary:
-        s3_var = dict(configs.items('s3_var'))
+        import os
 
         # Initiating variables:
-        self.config_file = config_file
         self.bucket_name = bucket_name
+        self.aws_key = s3_conf_dict['aws_access_key_id']
+        self.aws_secret = s3_conf_dict['aws_secret_access_key']
 
-        self.aws_key = s3_var['aws_access_key_id']
-        self.aws_secret = s3_var['aws_secret_access_key']
         # opening a connection:
         self.connection = boto.connect_s3(self.aws_key, self.aws_secret)
         self.bucket = self.connection.get_bucket(self.bucket_name)
         self.status = 'Active'
+
 
     def __str__(self):
         ''' Initiating a str method that gives meta information on the instance:
@@ -55,40 +59,62 @@ class PyS3:
         sent5 = 'Connection is :' + self.status
         return '\n' + sent1 + '\n' + sent2 + '\n' + sent3 + '\n' + sent4 + '\n' + sent5
 
-    def ListFolder(self, folder):
+
+    def list_folder(self, folder):
         '''This method lists the files in a specified folder.
         '''
-
         for key in self.bucket.list(prefix='{}/'.format(folder), delimiter='/'):
             print(key.name)
 
-    def StreamFile(self, file_path):
+
+    def stream_flile(self, file_path):
         '''This method streams a file (that is specified within the file_path) from S3 connection.
         '''
-
         file_stream = self.bucket.get_key('{}'.format(file_path))
         return file_stream
 
-    def DwnldFile(self, S3_file_path, local_directory_path, file_name=False):
+
+    def read_lines(self, file_path, function_on_line):
+        '''This method streams and reads a file line by line.
+        '''
+        pipe = '' # initiating an empty queue
+        file_stream = self.bucket.get_key('{}'.format(file_path)) # getting key
+        file_stream.open() # opening a connection
+        for byte in file_stream: # itterating over bytes
+            pipe += byte
+            lines = pipe.split('\n')
+            temp = lines[-1]
+            pipe = lines.pop(-1)
+            for line in lines:
+                function_on_line(line)
+            pipe = temp
+
+
+    def s3_2local(self, s3_file_path, local_directory_path, file_name=None):
         '''This method downloads a specified S3_file_path to a specified local_file_path.
         '''
-
-        file2dwnld = self.bucket.get_key('{}'.format(S3_file_path))
-
+        file2dwnld = self.bucket.get_key('{}'.format(s3_file_path))
         keyString = str(file2dwnld.key).split('/')[-1]
         # check if file exists locally, if not: download it
-        if not file_name:
+        if not file_name: # if file name is not specified (None):
             if not os.path.exists(local_directory_path + keyString):
                 file2dwnld.get_contents_to_filename(local_directory_path + keyString)
             else:
                 print 'file already exist in local path'
-        else:
-            if not os.path.exists(local_directory_path + keyString):
+        else: # if file name is  specified :
+            if not os.path.exists(local_directory_path + file_name):
                 file2dwnld.get_contents_to_filename(local_directory_path + file_name)
             else:
                 print 'file already exist in local path'
 
-    def CloseConnection(self):
+
+    def local_2s3(self, S3_file_path, local_directory_path, file_name=False):
+        '''This method downloads a specified S3_file_path to a specified local_file_path.
+        '''
+        return 'Currently not operational, please contact developer'
+
+
+    def close_connection(self):
         ''' This method terminates the S3 connection.
         '''
         self.connection.close()
